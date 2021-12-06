e3ee137a25320c947d3d4e2480813a141647010c
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2021-12-06 09:37:13 +0530

    Update and rename Prog3Application.java to App.java

--- prog3/src/main/java/com/prog3/demo/App.java
+++ prog3/src/main/java/com/prog3/demo/App.java
@@ -8,7 +8,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
  
-public class Prog3Application {
+public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Program Started...");
