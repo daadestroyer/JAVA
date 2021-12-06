c2ef34a34c08f8e2a001538e5efce67d5ee8950a
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2021-12-07 00:07:00 +0530

    Update App.java

--- prog5/App.java
+++ prog5/App.java
@@ -1,15 +1,14 @@
-package com.prog5.rvce;
+package com.rvce.prog6;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context =  new ClassPathXmlApplicationContext("com/prog5/rvce/config.xml");
-		Student s1 = (Student) context.getBean("s1");
-		System.out.println(s1);
-		
-		Student s2 = (Student) context.getBean("s2");
-		System.out.println(s2);
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/rvce/prog6/config.xml");
+		Emp e1 = context.getBean("emp1",Emp.class);
+		System.out.println(e1);
+		 
+
 	}
 }
