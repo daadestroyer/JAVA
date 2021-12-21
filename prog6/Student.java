4452a11b8aa2b26e432c5377627dae5561130238
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2021-12-21 09:58:58 +0530

    Update and rename Address.java to Student.java

---
+++ prog6/Student.java
@@ -0,0 +1,61 @@
+package com.rvce.prog6;
+ 
+
+public class Student {
+
+	private int id;
+	private String name;
+	private String course;
+	
+	private Marks marks;
+
+	public Student( ) {
+		 
+	}
+	public Student(int id, String name, String course) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.course = course;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", course=" + course + ", marks=" + marks + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public Marks getMarks() {
+		return marks;
+	}
+
+	public void setMarks(Marks marks) {
+		this.marks = marks;
+	}
+
+	 
+
+}
