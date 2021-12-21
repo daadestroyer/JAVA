947c9e393e806796dff4c70d5f50d7665adbac36
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2021-12-21 10:00:30 +0530

    Update App.java

--- prog6/App.java
+++ prog6/App.java
@@ -1,14 +1,23 @@
 package com.rvce.prog6;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/rvce/prog6/config.xml");
-		Emp e1 = context.getBean("emp1",Emp.class);
-		System.out.println(e1);
+		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+		Student s1 = context.getBean("getStudent",Student.class);
+		Marks m1 = context.getBean("getMarks",Marks.class);
 		
-
+		s1.setId(101);
+		s1.setName("shubham");
+		s1.setCourse("MCA");
+		
+		m1.setMarks1(100);
+		m1.setMarks2(200);
+		s1.setMarks(m1);
+		
+		
+		System.out.println(s1);
 	}
 }
